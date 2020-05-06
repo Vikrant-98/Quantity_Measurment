ca8f884bd6712182824cbc6dcde7ebc4f2c251eb
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-05-06 21:58:40 +0530

    Test For Null Value Exception

--- NUnitTestQuantity_Measurement/UnitTest1.cs
+++ NUnitTestQuantity_Measurement/UnitTest1.cs
@@ -5,15 +5,25 @@ namespace Tests
 {
     public class Tests
     {
-        ConversionType obj = new ConversionType();
+        ConversionType type = new ConversionType();
         /// <summary>
         /// TC-1.1 Test Case Compare with Zero
         /// </summary>
         [Test]
         public void Given_Feet_Convert_To_Inch_Zero_Input()
         {
-            double actual = obj.Feet_to_Inch(0);
+            double actual = type.Feet_to_Inch(0);
             double expected = 0;
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// TC-1.2 Test for Null Exception
+        /// </summary>
+        [Test]
+        public void Given_Input_Check_For_Null_Value_Feet_to_Inch()
+        {
+            string actual = type.Convert("", 1);
+            string expected = "Enter Null Vlaue";
             Assert.AreEqual(expected, actual);
         }
     }
