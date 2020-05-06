e4b04433b91472138e7b2765504a5de86cae2d5c
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-05-06 21:50:32 +0530

    Zero Input to Feet return Zero Inch

--- NUnitTestQuantity_Measurement/UnitTest1.cs
+++ NUnitTestQuantity_Measurement/UnitTest1.cs
@@ -1,9 +1,20 @@
 using NUnit.Framework;
+using Quantity_Measurement;
 
 namespace Tests
 {
     public class Tests
     {
-        
+        ConversionType obj = new ConversionType();
+        /// <summary>
+        /// TC-1.1 Test Case Compare with Zero
+        /// </summary>
+        [Test]
+        public void Given_Feet_Convert_To_Inch_Zero_Input()
+        {
+            double actual = obj.Feet_to_Inch(0);
+            double expected = 0;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
