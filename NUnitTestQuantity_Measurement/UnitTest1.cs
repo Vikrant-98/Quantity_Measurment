27be56d6c4b138b321880e99a99c6eb8b0fdde95
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-05-06 22:56:04 +0530

    Not Equal Check

--- NUnitTestQuantity_Measurement/UnitTest1.cs
+++ NUnitTestQuantity_Measurement/UnitTest1.cs
@@ -57,7 +57,7 @@ namespace Tests
             Assert.AreEqual(expected, actual);
         }
         /// <summary>
-        /// TC-2.1 Test Case Compare with Zero
+        /// TC-1.6 Test Case Compare with Zero
         /// </summary>
         [Test]
         public void Given_Inch_Convert_To_Feet_Zero_Input()
@@ -66,7 +66,7 @@ namespace Tests
             double expected = 0;
             Assert.AreEqual(expected, actual);
         }/// <summary>
-        /// TC-2.2 Test Case to check Null Value
+        /// TC-1.7 Test Case to check Null Value
         /// </summary>
         [Test]
         public void Given_Input_Check_For_Null_Value_Inch_to_Feet()
@@ -76,7 +76,7 @@ namespace Tests
             Assert.AreEqual(expected, actual);
         }
         /// <summary>
-        /// TC-2.3 Test for Invalid Reference format Exception
+        /// TC-1.8 Test for Invalid Reference format Exception
         /// </summary>
         [Test]
         public void Given_Input_Check_For_Ref_Check_Inch_to_Feet()
@@ -86,7 +86,7 @@ namespace Tests
             Assert.AreEqual(expected, actual);
         }
         /// <summary>
-        /// TC-2.4 Test for Invalid type check Exception
+        /// TC-1.9 Test for Invalid type check Exception
         /// </summary>
         [Test]
         public void Given_Input_Check_For_Invalid_Type_Inch_to_Feet()
@@ -96,7 +96,7 @@ namespace Tests
             Assert.AreEqual(expected, actual);
         }
         /// <summary>
-        /// TC-2.5 Given value input gives correct output
+        /// TC-1.10 Given value input gives correct output
         /// </summary>
         [Test]
         public void Given_Input_Check_For_Value_Check_for_Equality_Inch_to_Feet()
@@ -105,5 +105,16 @@ namespace Tests
             double expected = 0.3;
             Assert.AreEqual(expected, actual);
         }
+        /// <summary>
+        /// TC-1.11 Value check for 1 Feet Not Equal 1 Inch
+        /// </summary>
+        [Test]
+        public void Given_Input_Check_For_Value_Check_for_Not_Equality_1_Feet_to_1_Inch()
+        {
+            double actual = type.Feet_to_Inch(1);
+            double expected = 1;
+            Assert.AreNotEqual(expected, actual);
+        }
+        
     }
 }
